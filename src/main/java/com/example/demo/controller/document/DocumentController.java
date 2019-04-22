90e01d05737a59dba484936b697de2dd9b9f6187
AlinaZ <zagitova92@gmail.com@>
2019-04-22 21:25:06 +0500

    empty controllers created

---
+++ src/main/java/com/example/demo/controller/document/DocumentController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller.document;
+
+//import io.swagger.annotations.ApiOperation;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+@Validated
+@RestController
+//@RequestMapping(value = "/", produces = APPLICATION_JSON_VALUE)
+
+public class DocumentController {
+
+    // @ApiOperation("Проверка доступности приложения")
+    @RequestMapping(value = "/ping", method = {GET, POST})
+    public String ping() {
+        return "pong";
+    }
+
+   /* @GetMapping("/test/{test}")
+    public String test(@PathVariable @Size(min = 5, max = 100) String test) {
+        return "OK";
+    }*/
+}
